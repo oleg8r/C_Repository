78597835645720360dc97725bb72f3a2c5444e65
Oleg-Sav <62072132+Oleg-Sav@users.noreply.github.com>
2020-07-09 13:18:42 -0400

    Add files via upload
    File Name: calc.c
    | Purpose: To calculate the result based on the operation and numbers chosen by the user
    | Documentation:
    |     Ask the user to choose an arithmetical operation type from the list
    |     and let the user enter two numbers. Calculate the result of the arithmetical
    |     operation on the two entered numbers and show the result to the user.
    |
    | Revision History:
    | Date               Name                 Revision
    | --------------     -----------------     -------------------------------
    | 2020-06-25   Oleg Savelev     created

---
+++ DivisorCalc.c
@@ -0,0 +1,53 @@
+/*===========================================================
+|
+| File Name: DivisorCalc.c
+| Purpose: To print out all the divisors of a user entered number
+| Documentation:
+|     Ask the user to enter a positive number or 0 to end. Calculate and print
+|     out all the divisors of the entered number, plus the number of printed
+|     divisors and their sum.
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     -------------------------------
+| 2020-07-07         Oleg Savelev          created
+|
+|===========================================================*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+  int number = 0;     //User’s number
+  int divisor = 1;    //A divisor
+  int counter = 0;    //A number of divisors
+  int sum = 0;      //A sum of divisors
+
+  printf("This program identifies and displays divisors of a given number.\n");
+  printf("Developed as an IPC144 project. \n");
+  printf("\nEnter a positive number. Enter 0 to end: ");
+  scanf("%d", &number);
+
+  if (number > 0)
+  {
+    for (divisor = 1; divisor <= number; divisor = divisor + 1)
+      if (number % divisor == 0)
+      {
+        printf("%d ", divisor);
+        counter = counter + 1;
+        sum = sum + divisor;
+      }
+  printf("\n%d divisors", counter);
+  printf("\nsum of divisors: %d\n", sum);
+  }
+  else if (number == 0)
+  {
+    printf("Thanks and bye!\n");
+  }
+  else
+  {
+    printf("Invalid input\n");
+  }
+}
