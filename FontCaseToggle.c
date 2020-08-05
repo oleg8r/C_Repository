46950d5c4702dc3bebfc8c8de2d989d0917bb450
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-04 22:18:45 -0400

    Add files via upload
    This program uses the function that gets a string as aparameter and prints every lowercase character as an uppercase one and vise versa.

---
+++ FontCaseToggle.c
@@ -0,0 +1,58 @@
+/*===========================================================
+|
+| File Name: FontCaseToggle.c
+| Purpose:
+|	To print every lowercase character as an uppercase one and
+|	vise versa.
+|
+| Documentation:
+|	This program uses the function that gets a string as a
+|	parameter and prints every lowercase character as an
+|	uppercase one and vise versa.
+|	
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-07-29         Oleg Savelev          created
+|
+|===========================================================*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define ARR_SIZE 200
+
+void printToggled(char * myStr);
+
+int main(void)
+{
+   char * myStr;
+
+   printf("Please enter your text here\n");
+   gets(myStr);
+   printToggled(myStr);
+   printf("Your text with toggled case: %s\n", myStr);
+   return 0;
+}
+
+
+void printToggled(char * myStr)
+{
+for(int index = 0; index < strlen(myStr); index++)
+
+   while (myStr[index] != '\0')
+	{
+    if(myStr[index] >= 'a' && myStr[index] <= 'z')
+      {
+        myStr[index] = myStr[index] - 32;
+      }
+       
+	if(myStr[index] >= 'A' && myStr[index] <= 'Z')
+      {
+        myStr[index] = myStr[index] + 32;
+      }
+   }
+}
