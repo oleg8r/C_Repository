30a83b4b34378cf0be5ca74b7bb156323fb30339
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-06 23:15:06 -0400

    Update LcmCalc.c

--- LcmCalc.c
+++ LcmCalc.c
@@ -2,11 +2,13 @@
 |
 | File Name: LcmCalc.c
 | Purpose:
-|	To get two integers from the user and calculate and print  |	their least common multiplier or LCM. LCM is the smallest 
+|	To get two integers from the user and calculate and print
+|	their least common multiplier or LCM. LCM is the smallest 
 |	number that divides both given integers.
 |
 | Documentation:
-|	This program gets two numbers from the user, calls a       |	function to calculate their gcd and then uses the formula 
+|	This program gets two numbers from the user, calls a 
+|	function to calculate their gcd and then uses the formula 
 |	lcm(x,y) = xy/gcd(x,y) to calculate and display their LCM.
 |	
 |
@@ -33,7 +35,7 @@ int main()
  	printf("Enter two positive integer numbers: ");
  	scanf("%d %d", &num1, &num2);
 	gcdVar = gcd(num1, num2);
-	lcmVar = num1 * num2 / gcdVar;
+	lcmVar = num1 * num2 / gcdVar; // Using the formula lcm(x,y) = xy/gcd(x,y)
  	printf("LCM(%d,%d) = %d\n", num1, num2, lcmVar);
 }
 
@@ -42,6 +44,7 @@ int gcd(int num1, int num2)
 {
     int gcdCandidate ;
     if (num1 < num2) // To pick a candidate for GCD to be the smaller of the two integer numbers
+
         gcdCandidate = num1;
     else
         gcdCandidate = num2;
