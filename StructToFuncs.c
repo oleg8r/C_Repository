f30ced18ebdf5866a615df480b9cbc5b9f1f150d
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-18 16:51:28 -0400

    Add files via upload
    This program displays today's calendar date.

---
+++ StructToFuncs.c
@@ -0,0 +1,49 @@
+/*===========================================================
+|
+| File Name: ReverseArray.c
+| Purpose:
+|	To practice passing structure to functions.
+|
+| Documentation:
+|	This program displays today's calendar date.
+|	
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     -------------------
+| 2020-08-09         Oleg Savelev          created
+|
+|===========================================================*/
+
+
+/* To display today's calendar date */
+
+#include <stdio.h>
+
+struct date {
+        int year;
+        int month;
+        int day;
+    };
+
+void readDate(struct date *);
+void printDate(struct date);
+
+int main(void) {
+	struct date today;
+	readDate(&today);
+	printDate(today);
+	return 0;
+}
+
+void readDate(struct date * today) {
+  scanf("%d ", &(*today).year);
+  scanf("%d ", &(*today).month);
+  scanf("%d ", &(*today).day);
+}
+
+void printDate(struct date today) {
+  printf("%02d/", today.month);
+  printf("%02d/", today.day);
+  printf("%04d", today.year);
+}
