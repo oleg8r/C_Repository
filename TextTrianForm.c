983e5b374339cdf2f7c5445ad9d98c03ae87df68
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-21 11:33:37 -0400

    Update TextTrianForm.c
    Fixed description

--- TextTrianForm.c
+++ TextTrianForm.c
@@ -4,16 +4,16 @@
 | File Name: TextTrianForm.c
 | Purpose: To print a user string in the triangle format.
 | Documentation:
-|	The program using its function gets a string from the user 
-|	up to length 20 and prints it in the triangle format. For 
-|	example, if the user enters “Hello” the code should print:
-|
+|	The program's function gets a string from the user
+|	up to length 20 and prints it in the triangle format.
+|	For example, if the user enters "Hello” the code should
+|	print:
 |	H
 |	He
 |	Hel
 |	Hell
 |	Hello
-|	
+|
 |
 | Revision History:
 | Date               Name                  Revision
