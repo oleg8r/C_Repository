ecbe82bb816ac56fb84d3c0982c237b4308f8e0f
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-21 11:21:33 -0400

    Add files via upload
    This program  first reads the total number of names (an integer) in order to know how many index cards need to be processed altogether.	Next, for each index card, this program reads a patron's first name and last name and then displays these names correctly, that is on one line, the last name followed by one space, followed by the first name.  This program prints the reversed name immediately after reading the patron's names.

---
+++ StrSwap.c
@@ -0,0 +1,40 @@
+/*===========================================================
+|
+| File Name: StrSwap.c
+| Purpose:
+|	To practice swapping first and last name using strings.
+|
+| Documentation:
+|	This program  first reads the total number of names (an
+|	integer) in order to know how many index cards need to be
+|	processed altogether.	Next, for each index card, this
+|	program reads a patron's first name and last name and
+|	then displays these names correctly, that is on one line,
+|	the last name followed by one space, followed by the first
+|	name.  This program prints the reversed name immediately
+|	after reading the patron's names.	
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ------------------
+| 2020-08-18         Oleg Savelev          created
+|
+|===========================================================*/
+
+
+#include <stdio.h>
+#define MAX_ARR_SIZE 101
+
+int main(void) {
+    int count;
+    char firstName[MAX_ARR_SIZE];
+    char lastName[MAX_ARR_SIZE];
+
+    scanf("%d", &count);
+    for (int i=1; i<=count; i++) {
+      scanf("\n%s %s", firstName, lastName);
+      printf("%s %s\n", lastName, firstName);
+    }
+    
+    return 0;
+}
