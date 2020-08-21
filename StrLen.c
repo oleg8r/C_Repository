84e845952aa5505278c8d4983fee2398832311d6
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-20 22:40:26 -0400

    Add files via upload
    This program shows if the name has even number of letters or odd number of letters.

---
+++ StrLen.c
@@ -0,0 +1,34 @@
+/*===========================================================
+|
+| File Name: StrLen.c
+| Purpose:
+|	To practice string lengths.
+|
+| Documentation:
+|	This program shows if the name has even number of letters
+|	or odd number of letters.
+|	
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ------------------
+| 2020-08-18         Oleg Savelev          created
+|
+|===========================================================*/
+
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_ARR_SIZE 51
+
+int main(void) {
+  char name[MAX_ARR_SIZE];
+  
+  printf("Please enter the name: ");
+  scanf("%s", name);
+    if (strlen(name)%2==0)
+      printf("Even number of letters\n");
+    else (printf("Odd number of letters\n"));
+        
+    return 0;
+}
