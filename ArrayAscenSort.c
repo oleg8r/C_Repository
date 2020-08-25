07da7e0cc0e802ddc12e9cc68e778f6f8b641b06
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-08-25 13:49:23 -0400

    Update ArrayAscenSort.c
    Formatting fixed

--- ArrayAscenSort.c
+++ ArrayAscenSort.c
@@ -3,8 +3,7 @@
 /*===========================================================
 |
 | File Name: ArrayAscenSort.c
-| Purpose:
-|	To sort an array in a ascending order and to show the
+| Purpose: To sort an array in a ascending order and to show the
 |	number of elements that did not change their positions
 |	after the sorting.
 |
