1fc9d791effeb2f2c09b812be32550ee5d8b7c5d
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-03 12:36:29 -0400

    Update ArrayManipul.c

--- ArrayManipul.c
+++ ArrayManipul.c
@@ -195,7 +195,7 @@ void sortArrayDesc(float *arr, int size)
 {  
     int var1, var2;
     for (var1 = 0; var1 < size - 1; var1++)
-        for (var2 = 0; var2 < size - var1 - 1; var2 = var2 + 1)  
+        for (var2 = 0; var2 < size - var1 - 1; var2++)  
             if (arr[var2] < arr[var2 + 1])  
                 swap(&arr[var2], &arr[var2 + 1]);  
 }
