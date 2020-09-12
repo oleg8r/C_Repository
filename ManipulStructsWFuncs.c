89274f169565a39e4fab5228647addb92f60160f
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-12 10:55:32 -0400

    Update ManipulStructsWFuncs.c

--- ManipulStructsWFuncs.c
+++ ManipulStructsWFuncs.c
@@ -2,7 +2,7 @@
 |
 | File Name: ManipulStructsWFuncs.c
 | Purpose:
-|	To practice manipulation structures with functions.
+To practice manipulation structures with functions.
 |
 | Documentation:
 |	This program manipulates structures with functions.
