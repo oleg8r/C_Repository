a2f4da4b9ef16316f6ea95ec9ada630cc6a2accd
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-20 03:01:00 -0400

    Update PrimeNumCalc.c

--- PrimeNumCalc.c
+++ PrimeNumCalc.c
@@ -59,7 +59,7 @@ int isPrime(unsigned num)   // The function to return 1 if the number is prime a
 	unsigned divisor = 1;    // A divisor
   	int numOfDiv = 0;    // A number of divisors
 
-	for (divisor = 1; divisor <= num ; divisor = divisor + 1)
+	for (divisor = 1; divisor <= num ; divisor++)
      		if (num % divisor == 0)
      	  		numOfDiv = numOfDiv + 1;
 
