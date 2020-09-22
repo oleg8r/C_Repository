96d3c445b7500c52932859528073cf5cdf49192a
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-22 18:10:36 -0400

    Update PtrsAgeElixir.c

--- PtrsAgeElixir.c
+++ PtrsAgeElixir.c
@@ -25,7 +25,6 @@ int main(void) {
 
     ageptr = &age; // add a line here that stores the address of age in ageptr  
     printf("The secret address is... ");
-
     printf("%p\n", ageptr); // add a line here that prints out the address stored in ageptr  
     
     printf("Now take three drops of the magic elixir. \n");
