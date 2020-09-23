e5522c049fc7f2a81dcc47886cb635f40522c821
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-23 17:27:05 -0400

    Update PtrsAgeElixir.c

--- PtrsAgeElixir.c
+++ PtrsAgeElixir.c
@@ -26,7 +26,6 @@ int main(void) {
     printf("%p\n", ageptr); // add a line here that prints out the address stored in ageptr  
     printf("Now take three drops of the magic elixir. \n");
     *ageptr = *ageptr - 5;  // add a line that uses only ageptr to lower the age by 5 years
-    
 
     printf("Did the elixir work? You are %d years old!", age);
 
