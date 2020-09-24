a02862649214877ef4b29e839021bdbd303b67af
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-09-24 00:10:55 -0400

    Update PtrsAgeElixirUpd.c

--- PtrsAgeElixirUpd.c
+++ PtrsAgeElixirUpd.c
@@ -7,7 +7,7 @@
 |	his age by 10 years if he's 21 and older, and twice
 |	multiplies his age if he's younger than 21.
 |	
-|
+
 | Revision History:
 | Date               Name                  Revision
 | --------------     -----------------     -------------------
