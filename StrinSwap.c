653e52a113258d0698c277054ac73c90a8a740fc
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-07 16:43:28 -0400

    Update StrinSwap.c

--- StrinSwap.c
+++ StrinSwap.c
@@ -3,7 +3,7 @@
 | File Name: StrinSwap.c
 | Purpose: To practice swapping first and last name using strings.
 | Documentation:
-|	This program  first reads the total number of names (an
+This program  first reads the total number of names (an
 |	integer) in order to know how many index cards need to be
 |	processed altogether.	Next, for each index card, this
 |	program reads a patron's first name and last name and
