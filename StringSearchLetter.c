e13745b3a58c6da1fbb68d7354040fc07f5c1c57
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-10 00:43:28 -0400

    Update StringSearchLetter.c

--- StringSearchLetter.c
+++ StringSearchLetter.c
@@ -3,7 +3,8 @@
 | File Name: StringSearchLetter.c
 | Purpose: To find where a specific letter is contained in a
 |   word.
-| Documentation: This program shows what half of the word contains a 
+| Documentation: This program shows what half of the word contains
+a 
 |	specific letter. If there is no specific letter in the word at all, the 
 | program also says about this. The word entered should not have more than
 | 50 letters.
