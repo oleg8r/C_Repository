0a0c54ebdf1dbc51dff13c5bf6baa62fbac56158
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-12 12:19:47 -0400

    Update StringSearchLetter.c

--- StringSearchLetter.c
+++ StringSearchLetter.c
@@ -6,7 +6,7 @@
 | Documentation: This program shows what half of the word contains
 |   a specific letter. If there is no specific letter in the word
 |   at all, the | program also says about this. The word entered
-|should not have more than
+|   should not have more than
 | 50 letters.
 |
 | Revision History:
