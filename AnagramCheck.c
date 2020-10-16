ce17a5f06f21ca5dfa40c49049a8969daa58eded
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-15 20:05:05 -0400

    Update AnagramCheck.c

--- AnagramCheck.c
+++ AnagramCheck.c
@@ -3,7 +3,8 @@
 | File Name: AnagramCheck.c
 | Purpose: To check if two words are anagrams
 | Documentation: This program asks users to enter two words,
-|	one after the other and checks if these words are anagrams (permutations
+|	one after the other and checks if these words are
+anagrams (permutations
 |	of the same letters).
 |	
 | Revision History:
