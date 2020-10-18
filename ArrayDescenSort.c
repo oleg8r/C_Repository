390181c098de9ebf2648349029b7f6da47556e45
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-18 02:22:59 -0400

    Update ArrayDescenSort.c

--- ArrayDescenSort.c
+++ ArrayDescenSort.c
@@ -3,7 +3,7 @@
 | File Name: ArrayDescenSort.c
 | Purpose: To sort an array in a descending order and to show
 |	the number of elements that did not change their
-|positions
+|	positions
 |	after the sorting.
 | Documentation: This program uses the function that gets an
 |	array as a parameter, sorts it in a descending fashion
