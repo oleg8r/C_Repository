611497051aa69df3078896319002c2dd933b8ec8
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-20 22:41:43 -0400

    Update DivisorCalc.c

--- DivisorCalc.c
+++ DivisorCalc.c
@@ -3,7 +3,7 @@
 | File Name: DivisorCalc.c
 | Purpose: To print out all the divisors of a user entered
 |          number.
-| Documentation: Ask the user to enter a positive number or 0 to end.
+| Documentation: Ask the user to enter a positive number or 0to end.
 |	Calculate and print out all the divisors of the entered
 |	number, plus the number of printed divisors and their sum.
 |
