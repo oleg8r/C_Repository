5305518a6c2d05267755cde9a99dd24ca1e839c0
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-21 18:22:33 -0400

    Update DivisorCalc.c

--- DivisorCalc.c
+++ DivisorCalc.c
@@ -5,7 +5,8 @@
 |   number.
 | Documentation: Ask the user to enter a positive number or 0
 |   to end. Calculate and print out all the divisors of 
-|   the entered number, plus the number of printed divisors and their sum.
+|   the entered number, plus the number of printed divisors
+and their sum.
 |
 | Revision History:
 | Date               Name                  Revision
