c20717e5b8d50e97e7b37161d263c03c3ceeddb2
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-22 18:39:13 -0400

    Update DivisorCountFunc.c

--- DivisorCountFunc.c
+++ DivisorCountFunc.c
@@ -3,7 +3,7 @@
 | File Name: DivisorCountFunc.c
 | Purpose: To return the number of divisors of an integer number
 |	(by using a function).
-| Documentation:The function called unsigned divisorCount (unsigned num)
+| Documentation: The function called unsigned divisorCount (unsigned num)
 |	gets an integer number as an input parameter and returns 
 |	the number of divisors of that number. For example, if you
 |	send 15 to the function, it will return 4 as 15 has 4
