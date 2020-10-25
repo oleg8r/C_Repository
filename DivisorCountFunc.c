ad3948a95c1edf8aaedddd84a53325fc4b4814b4
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-24 20:05:55 -0400

    Update DivisorCountFunc.c

--- DivisorCountFunc.c
+++ DivisorCountFunc.c
@@ -6,7 +6,8 @@
 | Documentation: The function called unsigned divisorCount
 |	(unsigned num) gets an integer number as an input
 |	parameter and returns the number of divisors of that
-|	number. For example, if you send 15 to the function,it will return 4 as 15 has 4
+|	number. For example, if you send 15 to the function,
+it will return 4 as 15 has 4
 |	divisors: 1, 3, 5, 15. The function will return 2 for 17
 |	because 17 only has two divisors.
 |
