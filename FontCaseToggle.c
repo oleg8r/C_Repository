a01160ef7b51161e3b8c20b41b467580b2a39958
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-28 00:05:06 -0400

    Update FontCaseToggle.c

--- FontCaseToggle.c
+++ FontCaseToggle.c
@@ -5,8 +5,7 @@
 |	one andvise versa.
 | Documentation: This program uses the function that gets
 |	a string as a parameter and prints every lowercase
-|character as an
-uppercase one and vise versa.
+|character as anuppercase one and vise versa.
 |	
 | Revision History:
 | Date               Name                  Revision
