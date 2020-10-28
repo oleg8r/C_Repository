978ff60c63f9adc812e8b0f6077492dd83985869
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-28 00:04:21 -0400

    Update FontCaseToggle.c

--- FontCaseToggle.c
+++ FontCaseToggle.c
@@ -4,7 +4,8 @@
 | Purpose: To print every lowercase character as an uppercase
 |	one andvise versa.
 | Documentation: This program uses the function that gets
-|	a string as a parameter and prints every lowercasecharacter as an
+|	a string as a parameter and prints every lowercase
+character as an
 |	uppercase one and vise versa.
 |	
 | Revision History:
