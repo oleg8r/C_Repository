263d394e8afdecedb50829cf59a8272b1b24344f
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-01 12:54:04 -0500

    Update FourOperCalc.c

--- FourOperCalc.c
+++ FourOperCalc.c
@@ -4,7 +4,7 @@
 | Purpose: To calculate the result based on the operation and
 |	numbers chosen by the user
 | Documentation: Ask the user to choose an arithmetical operation
-|  type from the list and let the user enter two numbers. Calculate the
+|  type from the list and let the user enter two numbers.Calculate the
 |	result of the arithmetical operation on the two entered
 |	numbers and show the result to the user.
 |
