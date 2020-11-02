8e5ac5d8d14643c6bb2850839b3186b8ef998e73
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-02 13:20:40 -0500

    Update FourOperCalc.c

--- FourOperCalc.c
+++ FourOperCalc.c
@@ -5,7 +5,7 @@
 |	numbers chosen by the user
 | Documentation: Ask the user to choose an arithmetical operation
 |  type from the list and let the user enter two numbers.
-|  Calculate the |result of the arithmetical operation on the two entered
+|  Calculate the result of the arithmetical operation on the two entered
 |	numbers and show the result to the user.
 |
 | Revision History:
