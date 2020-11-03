72f1555e7163e5de3c3a9a7891f9a235f2e78ed0
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-03 13:39:06 -0500

    Update FourOperCalc.c

--- FourOperCalc.c
+++ FourOperCalc.c
@@ -2,7 +2,7 @@
 |
 | File Name: FourOperCalc.c
 | Purpose: To calculate the result based on the operation and
-|	numbers chosen by the user
+|	numbers chosen by the user.
 | Documentation: Ask the user to choose an arithmetical operation
 |  type from the list and let the user enter two numbers.
 |  Calculate the result of the arithmetical operation on the two
