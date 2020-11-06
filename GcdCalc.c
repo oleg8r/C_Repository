a6aa333d4e086bc60982abd37507a89f9212809d
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-06 10:52:04 -0500

    Update GcdCalc.c

--- GcdCalc.c
+++ GcdCalc.c
@@ -7,7 +7,7 @@
 |	by.
 | Documentation: This program picks a candidate for GCD to be
 |	the smaller
-|of the two numbers. It checks if that candidate satisfies
+of the two numbers. It checks if that candidate satisfies
 |	the GCD condition. If not, it reduces the candidate by one
 |	and tries again. The first number that satisfies the
 |	conditions will be the GCD.
