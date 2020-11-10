3fa4713d429c623304bf47951a83339996fe71f5
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-10 12:04:25 -0500

    Update GuessNumGame.c

--- GuessNumGame.c
+++ GuessNumGame.c
@@ -3,7 +3,7 @@
 | File Name: GuessNumGame.c
 | Purpose: A guessing number game with a computer.
 | Documentation:
-|	A computer chooses a random number between 1 and 100,
+|A computer chooses a random number between 1 and 100,
 |	and a user tries to guess it with 10 attempts.
 |
 | Revision History:
