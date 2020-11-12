a7bab13a32952a162bdde48f643e71eda23a4693
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-12 10:35:32 -0500

    Update GuessNumGame.c

--- GuessNumGame.c
+++ GuessNumGame.c
@@ -33,7 +33,7 @@ int main(void)
   r = rand() % 100 + 1;
   printf("OK, I made my mind!");
   
-  for (counter = 0; counter < 10; counter++) // cou
+  for (counter = 0; counter < 10; counter++) // coun
   {
     printf("\nWhat is your guess? > ");
     scanf("%d", &guess);
