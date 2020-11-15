b610517eb023c694da7a265062ac2ad34375fb70
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-15 14:20:32 -0500

    Update LcmCalc.c

--- LcmCalc.c
+++ LcmCalc.c
@@ -3,7 +3,7 @@
 | File Name: LcmCalc.c
 | Purpose: To get two integers from the user and calculate and
 |	print their least common multiplier or LCM. LCM is
-|the smallest 
+|	the smallest 
 |	number that divides both given integers.
 | Documentation:
 |	This program gets two numbers from the user, calls a 
