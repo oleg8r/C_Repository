b08e0b3ea3aa8c28aa39e979ff46ae1046875ee3
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-11-23 22:30:25 -0500

    Update PrimeNumCalc.c

--- PrimeNumCalc.c
+++ PrimeNumCalc.c
@@ -5,7 +5,7 @@
 |	the user's positive integer number.
 | Documentation: To print all the prime numbers smaller and
 |	equal to the user's positive integer number, the function
-|	int isPrime(unsigned num) gets a number as a parameter and
+|	int isPrime(unsigned num) gets a number as a parameterand
 |	returns 1 if the number is prime and returns 0 if the
 |	number is not prime.
 |	
