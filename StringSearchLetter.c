1468bbfa5fa4c56e0693686201b7d130a0e4f7ba
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-01-14 12:05:52 -0500

    Update StringSearchLetter.c

--- StringSearchLetter.c
+++ StringSearchLetter.c
@@ -37,7 +37,7 @@ int main(void)
     if(word[i]==letter)
     {
       position=i+1;
-      printf("Letter %c is in the position number %d in the word.\n",letter, position);
+      printf("Letter %c is in the position number %d in the word.\n", letter, position);
       if(strlen(word) % position ==0 && position <= strlen(word) /2 && position>0) //for even words
         printf("Letter %c is in the first half of the word.\n", letter);
         //return 1;
